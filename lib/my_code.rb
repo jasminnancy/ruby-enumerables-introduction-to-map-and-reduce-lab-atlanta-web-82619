b2a9aa0771f401a0ea42372eb4c2a47fc3deb65d
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-18 02:30:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,51 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+  i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    x = source_array[i] * -1
+    new_array.push(x)
+    i += 1
+  end
+  return new_array
+end
+
+def map_to_no_change(source_array)
+  i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    new_array.push(source_array[i])
+    i += 1
+  end
+  return new_array
+end
+
+def map_to_double(source_array)
+  i = 0
+  new_array = []
+  
+  while i <source_array.length do
+    x = source_array[i] * 2
+    new_array.push(x)
+  end
+  return new_array
+end
+
+def map_to_square(source_array)
+  
+end
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end
